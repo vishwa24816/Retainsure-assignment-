8142c0e9964d2e8f347df0bd48dcba81e20ce262
Vishwa Lingam <64265390+vishwa24816@users.noreply.github.com>
2025-07-23 13:12:45 +0530

    Update utils.py

--- messy-migration/src/utils.py
+++ messy-migration/src/utils.py
@@ -3,7 +3,6 @@ import bcrypt
 def hash_password(password):
     """Hashes a password using bcrypt."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
 def check_password(password, hashed_password):
     """Checks a password against a hashed password."""
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
