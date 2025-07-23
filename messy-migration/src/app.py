198889b39a1e266796b2aa9127cd289204c61f2e
Vishwa Lingam <64265390+vishwa24816@users.noreply.github.com>
2025-07-23 09:09:45 +0530

    Update app.py

--- messy-migration/src/app.py
+++ messy-migration/src/app.py
@@ -5,7 +5,6 @@ def create_app():
     app = Flask(__name__)
     app.register_blueprint(bp)
     return app
-
 if __name__ == '__main__':
     app = create_app()
     app.run(host='0.0.0.0', port=5000, debug=True)
